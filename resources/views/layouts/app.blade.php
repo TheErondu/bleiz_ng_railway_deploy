4816e1f2240d49c007b7fd7d4366315b4e174b96
Theerondu <erone007@gmail.com>
2025-09-20 13:43:03 +0100

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,10 +15,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
-      <link href="{{asset('build/assets/app.css')}}" rel="stylesheet">
+      {{-- <link href="{{asset('build/assets/app.css')}}" rel="stylesheet"> --}}
 
-    {{-- <!-- Vite Assets -->
-    @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <!-- Vite Assets -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @stack('styles')
 </head>
